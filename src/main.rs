dc3a011915073a027eae9c9c2aa8e927c39cd462
Kevin Trieu <kevin.trieu5813@gmail.com>
2022-11-22 20:01:27 -0500

    Add basic slash command support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+use std::env;
+
+use dotenv::dotenv;
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::application::interaction::Interaction;
+use serenity::model::application::interaction::InteractionResponseType;
+use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
+use serenity::{async_trait, prelude::*};
+
+struct Handler;
+
+fn test_command_register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("test")
+        .description("A test command for the UGO bot")
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "test" => "UGO II BOT TEST",
+                _ => !unimplemented!(),
+            };
+
+            let resp_result = command
+                .create_interaction_response(ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await;
+
+            if let Err(why) = resp_result {
+                println!("Could not respond to interaction! {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, _ready: Ready) {
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("No GUILD_ID variable in environment!")
+                .parse()
+                .expect("GUILD_ID is not an integer!"),
+        );
+
+        let create_result = guild_id
+            .set_application_commands(ctx.http, |commands| {
+                commands.create_application_command(|command| test_command_register(command))
+            })
+            .await;
+
+        if let Err(why) = create_result {
+            println!("Failed to create commands! {:?}", why);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token =
+        env::var("DISCORD_TOKEN").expect("No DISCORD_TOKEN found in environment variables!");
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Failed to create Discord client!");
+
+    if let Err(why) = client.start().await {
+        panic!("Failed to start client {:?}", why);
+    }
 }
