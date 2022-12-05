3b27bfd6d90cff562fd7aef716c733564dd95ace
Kevin Trieu <kevin.trieu5813@gmail.com>
2022-12-05 00:47:55 -0500

    Add balances command

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serenity::builder::{CreateApplicationCommand, CreateApplicationCommands};
 use serenity::client::Context;
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
@@ -5,7 +7,8 @@ use serenity::model::application::interaction::InteractionResponseType;
 use sqlx::SqlitePool;
 
 use crate::error::{Error, InnerError, WithContext};
-use crate::user;
+use crate::ugocoin::{self, Ugocoin};
+use crate::user::{self, User};
 
 // This is kinda trash, but async function pointers are actually quite difficult to do.
 // Instead, enjoy a bunch of string constants and a giant match table.
@@ -42,10 +45,75 @@ async fn test_command_run(
     Ok(())
 }
 
+const BALANCES_COMMAND_NAME: &str = "balances";
+
+fn balances_command_register(
+    command: &mut CreateApplicationCommand,
+) -> &mut CreateApplicationCommand {
+    command
+        .name(BALANCES_COMMAND_NAME)
+        .description("Display balances for all employees, in the name of financial transparency.")
+}
+
+struct BalanceInfo {
+    name: String,
+    balance: Ugocoin,
+    streak: i64,
+}
+
+async fn balances_command_run(
+    db: &SqlitePool,
+    context: &Context,
+    command: &ApplicationCommandInteraction,
+) -> Result<(), Error> {
+    let users = user::get_all_users(db).await?;
+
+    let mut balance_infos: Vec<BalanceInfo> = Vec::new();
+
+    for u in users {
+        let account = ugocoin::get_user_account(db, &u).await?;
+        balance_infos.push(BalanceInfo {
+            name: u.display_name,
+            balance: account.balance,
+            streak: u.streak,
+        });
+    }
+
+    let central_account = ugocoin::get_central_bank_account(db).await?;
+    balance_infos.push(BalanceInfo {
+        name: String::from("UGOcoin Central Bank"),
+        balance: central_account.balance,
+        streak: 0,
+    });
+
+    balance_infos.sort_by_cached_key(|info| info.balance);
+    balance_infos.reverse();
+
+    let mut content: String = String::from("Current UGOcoin balances:\n\n");
+
+    for info in balance_infos {
+        content += &format!(
+            "{}: {} (scrum streak {})\n",
+            info.name, info.balance, info.streak
+        );
+    }
+
+    command
+        .create_interaction_response(&context.http, |resp| {
+            resp.kind(InteractionResponseType::ChannelMessageWithSource)
+                .interaction_response_data(|data| data.content(content))
+        })
+        .await?;
+
+    Ok(())
+}
+
 pub fn register_commands(
     commands: &mut CreateApplicationCommands,
 ) -> &mut CreateApplicationCommands {
-    commands.create_application_command(|command| test_command_register(command))
+    commands
+        .create_application_command(test_command_register)
+        .create_application_command(balances_command_register)
 }
 
 pub async fn run_command(
@@ -56,6 +124,7 @@ pub async fn run_command(
 ) -> Result<(), Error> {
     match command_name {
         TEST_COMMAND_NAME => test_command_run(db, context, command).await,
+        BALANCES_COMMAND_NAME => balances_command_run(db, context, command).await,
         _ => Err(InnerError::CommandNotFound(command_name.to_string()).into()),
     }
 }
--- src/ugocoin.rs
+++ src/ugocoin.rs
@@ -5,6 +5,7 @@ use crate::{error::Error, user::User};
 use sqlx::SqlitePool;
 
 // Ugocoins are represented as a fixed-point number of ugocents.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Ugocoin(i64);
 
 impl Ugocoin {
@@ -36,10 +37,10 @@ pub struct UgocoinAccount {
     pub balance: Ugocoin,
 }
 
-async fn get_account_by_id(db: &SqlitePool, id: Option<i64>) -> Result<UgocoinAccount, Error> {
+pub async fn get_user_account(db: &SqlitePool, user: &User) -> Result<UgocoinAccount, Error> {
     let result = sqlx::query!(
         "SELECT id, user_id, balance from ugocoin_accounts WHERE user_id = ?",
-        id
+        user.id
     )
     .fetch_one(db)
     .await?;
@@ -51,11 +52,16 @@ async fn get_account_by_id(db: &SqlitePool, id: Option<i64>) -> Result<UgocoinAc
     })
 }
 
-pub async fn get_user_account(db: &SqlitePool, user: &User) -> Result<UgocoinAccount, Error> {
-    get_account_by_id(db, Some(user.id)).await
-}
-
 pub async fn get_central_bank_account(db: &SqlitePool) -> Result<UgocoinAccount, Error> {
     // The central bank account is the account with no user ID associated
-    get_account_by_id(db, None).await
+    let result =
+        sqlx::query!("SELECT id, user_id, balance from ugocoin_accounts WHERE user_id IS NULL",)
+            .fetch_one(db)
+            .await?;
+
+    Ok(UgocoinAccount {
+        id: result.id,
+        user_id: result.user_id,
+        balance: Ugocoin::from_ugocents(result.balance),
+    })
 }
